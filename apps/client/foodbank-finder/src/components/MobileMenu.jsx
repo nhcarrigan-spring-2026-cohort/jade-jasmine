9efdd42a82351cdb69a27804cde51ee5d95b55ef
Rosa Cabrera <mrcabreracorrea@gmail.com>
2026-02-04 15:25:01 -0300

    feat: implement responsive navbar with interactive mobile menu

--- apps/client/foodbank-finder/src/components/MobileMenu.jsx
+++ apps/client/foodbank-finder/src/components/MobileMenu.jsx
@@ -1,3 +1,19 @@
 import { useState } from "react";
 import { BsSearch, BsList, BsX } from "react-icons/bs";
 
+export default function MobileMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="mobile-menu-container">
+      <button onClick={() => setIsOpen(!isOpen)}>{isOpen  ? <BsX /> : <BsList />}</button>
+
+      {isOpen && (
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="#"><BsSearch />Search</a></li>
+      </ul>
+      )}
+    </div>
+  );
+}
