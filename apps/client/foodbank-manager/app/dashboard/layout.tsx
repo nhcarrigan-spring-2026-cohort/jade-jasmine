43ef90f1cce34925ef046f4f80a6c3120801d3b2
hayden.html <75038475+hayden-html@users.noreply.github.com>
2026-02-06 11:58:03 +0000

    added the dashboard page

--- apps/client/foodbank-manager/app/dashboard/layout.tsx
+++ apps/client/foodbank-manager/app/dashboard/layout.tsx
@@ -1,5 +1,3 @@
-import "./globals.css";
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -7,7 +5,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex h-dvh">
+          <nav className="min-w-60 h-full border-r-2 p-4 flex flex-col justify-between">
+            <div className="flex flex-col gap-4">
+              <p>Foodbank Account Name</p>
+              <p>Dashboard</p>
+              <p>Inventory</p>
+            </div>
+            <div className="flex flex-col gap-4 mb-12">
+              <p>Sign Out</p>
+            </div>
+          </nav>
+          <main>{children}</main>
+        </div>
+      </body>
     </html>
   );
 }
